98f862d00e5e804880d6d9ceb63b82d575688ac5
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-10-22 20:51:31 +0100

    working on tests for validation

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// Test block
+test.describe("Test the page inputs", () => {
+  // Before each test make sure the elements are on the page
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://127.0.0.1:5501/index.html");
+    await expect(
+      page.getByRole("heading", { name: "Code Challenge - Secret Number" })
+    ).toBeVisible();
+    await expect(page.getByText("Enter a number between 1 - 10")).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Guess number 0 of" })
+    ).toBeVisible();
+  });
+  test("Check results update on guess", async ({ page }) => {
+    await page.getByRole("spinbutton").click();
+    await page.getByRole("spinbutton").fill("5");
+    await page.getByRole("button", { name: "Enter Guess" }).click();
+    await expect(
+      page.getByText("Enter a number between 1 - 10")
+    ).not.toBeVisible();
+  });
+  test("Check number validation", async ({ page }) => {
+    await page.getByRole("spinbutton").click();
+    await page.getByRole("spinbutton").fill("11");
+    await page.getByRole("button", { name: "Enter Guess" }).click();
+    await expect(
+      page.getByText("Number needs to be in range of 1-10")
+    ).toBeVisible();
+  });
 });
