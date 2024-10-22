28d762971ecc72c34b142cd6975b7180db9d17ea
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2024-10-22 20:20:34 +0100

    added a function to handle the game reset

--- script.js
+++ script.js
@@ -18,28 +18,47 @@ guessNumber.textContent = 0;
 // Sets the number of guesses allowed to be made before the game stops
 const allowedNumberOfGuesses = 5;
 
-console.log(computerGuess);
-
+// This button starts the game function
 checkGuess.addEventListener("click", () => {
+  //   Takes the inner value of the input box and stores it as the users guess
   let userGuess = userInput.value;
+  //   Function to validate the number entered
   checkValidNumber(userGuess);
+  //   Updates the guess number on the page
   guessNumber.textContent = numberOfGuesses;
 });
 
+// Function to validate user number input
 function checkValidNumber(number) {
   if (number < 1 || number > 10) {
     results.textContent = "Number needs to be in range of 1-10";
     userInput.value = "";
   } else {
+    // If number in valid range run the game function
     runGame(number);
   }
 }
 
+// Reset the game
+function resetGame() {
+  // Clears the input box
+  userInput.value = "";
+  //   Gives the computer guess a new random value
+  computerGuess = Math.floor(Math.random() * 9) + 1;
+  //   Resets the number of guesses made
+  numberOfGuesses = 0;
+}
+
+// Game Function
 function runGame(userGuess) {
+  // First checks if the user hasnt used too many guesses
   if (numberOfGuesses < allowedNumberOfGuesses) {
     if (userGuess < computerGuess) {
+      // Increase the number of guesses by 1
       numberOfGuesses++;
+      //   Display a string with the users number guessed
       results.textContent = `You guessed ${userGuess}, the number is higher`;
+      //   Resets the input box to allow for another guess to be made
       userInput.value = "";
     } else if (userGuess > computerGuess) {
       numberOfGuesses++;
@@ -48,13 +67,10 @@ function runGame(userGuess) {
     } else {
       numberOfGuesses++;
       results.textContent = `Winner! the correct number was ${userGuess}, you got it on guess number ${numberOfGuesses}`;
-      userInput.value = "";
-      computerGuess = Math.floor(Math.random() * 9) + 1;
-      numberOfGuesses = 0;
+      resetGame();
     }
   } else {
     results.textContent = `You lose, the correct answer was ${computerGuess}`;
-    computerGuess = Math.floor(Math.random() * 9) + 1;
-    numberOfGuesses = 0;
+    resetGame();
   }
 }
